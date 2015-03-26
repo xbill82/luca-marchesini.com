6004d3413ad347c47246b8c76ddebf0bf7135152
Luca Marchesini <xbill82@gmail.com>
2015-03-27 00:44:09 +0100

    5 service tests implemented

--- tests/service.js
+++ tests/service.js
@@ -1,10 +1,13 @@
 var should = require('should'); 
 var request = require('supertest');  
 
-var url = 'sandbox/luca-marchesini.com/web-dev/API/';
+var url = 'localhost:8888/luca-marchesini.com/web-dev/API/';
+// var url = 'sandbox/luca-marchesini.com/web-dev/API/';
+
+var LIMIT = 2;
 
 describe('GET /gigs.php', function() {
-	it('Should return a list of gigs grouped by upcoming and old', function(done) {
+	it('Should return a list of gigs grouped by upcoming and old.', function(done) {
 		request(url)
 			.get('/gigs.php')
 			.expect('Content-Type', /json/)
@@ -12,42 +15,80 @@ describe('GET /gigs.php', function() {
 			.end(function(err, res) {
 				if (err)
 					throw err;
-				res.status.should.equal(200, "Gig list is retrieved successfully.");
-				res.body.length.should.not.equal(0, "Gig list is not empty.");
+				res.statusCode.should.equal(200, "Gig list is retrieved successfully.");
+				res.body.should.have.property('upcoming');
+				res.body.should.have.property('old');
 				done();
 			});
-		});
 	});
+});
 
-// describe('GET /articles/1', function() {
-// 	var idArticle = 1;
-// 	it('Should return an article', function(done) {
-// 		request(url)
-// 			.get('/articles/' + idArticle)
-// 			.expect('Content-Type', /json/)
-// 			.expect(200)
-// 			.end(function(err, res) {
-// 				if (err)
-// 					throw err;
-// 				res.status.should.equal(200,"no error code for get list");
-// 				res.body.should.have.property("id",idArticle," article id is ok ");
-// 				done();
-// 			});
-// 		});
-// 	});
+describe('GET /gigs.php?filter=old', function() {
+	it('Should return a non-empty list of old gigs.', function(done) {
+		request(url)
+			.get('/gigs.php?filter=old')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end(function(err, res) {
+				if (err)
+					throw err;
+				res.statusCode.should.equal(200, "Gig list is retrieved successfully.");
+				res.body.should.be.type('object');
+				res.body.length.should.be.above(0);
+				done();
+			});
+	});
+});
 
-// describe('GET /folders', function() {
-// 		it('Should return a list of folders', function(done) {
-// 			request(url)
-// 				.get('/folders')
-// 				.expect('Content-Type', /json/)
-// 				.expect(200)
-// 				.end(function(err, res) {
-// 					if (err)
-// 						throw err;
-// 					res.status.should.equal(200,"no error code for get list");
-// 					res.body.length.should.not.equal(0,"we found some folders");
-// 					done();
-// 			});
-// 		});
-// 	});
+describe('GET /gigs.php?filter=old&limit=' + LIMIT, function() {
+	it('Should return a list of ' + LIMIT + ' old gigs.', function(done) {
+		request(url)
+			.get('/gigs.php?filter=old&limit=' + LIMIT)
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end(function(err, res) {
+				if (err)
+					throw err;
+				res.statusCode.should.equal(200, "Gig list is retrieved successfully.");
+				res.body.should.be.type('object');
+				res.body.length.should.be.above(0);
+				res.body.length.should.not.be.above(LIMIT);
+				done();
+			});
+	});
+});
+
+describe('GET /gigs.php?filter=some', function() {
+	it('Should return a non-empty list of gigs.', function(done) {
+		request(url)
+			.get('/gigs.php?filter=some')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end(function(err, res) {
+				if (err)
+					throw err;
+				res.statusCode.should.equal(200, "Gig list is retrieved successfully.");
+				res.body.should.be.type('object');
+				res.body.length.should.be.above(0);
+				done();
+			});
+	});
+});
+
+describe('GET /gigs.php?filter=some&limit=' + LIMIT, function() {
+	it('Should return a list of ' + LIMIT + ' gigs.', function(done) {
+		request(url)
+			.get('/gigs.php?filter=some&limit=' + LIMIT)
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end(function(err, res) {
+				if (err)
+					throw err;
+				res.statusCode.should.equal(200, "Gig list is retrieved successfully.");
+				res.body.should.be.type('object');
+				res.body.length.should.not.be.above(LIMIT);
+				res.body.length.should.be.above(0);
+				done();
+			});
+	});
+});
