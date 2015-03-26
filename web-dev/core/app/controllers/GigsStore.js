36380a67865db8e648d51f8d60f901f7d5061562
lma <lma@amisw.com>
2015-03-26 13:42:34 +0100

    Upcoming dates in Home are now fetched from the Store using Deferreds. Homelayout shows a tiny loader while fetching.

--- web-dev/core/app/controllers/GigsStore.js
+++ web-dev/core/app/controllers/GigsStore.js
@@ -1,11 +1,11 @@
-define(['require', 'backbone.marionette', 'collections/Gigs'],
+define(['require', 'marionette', 'collections/Gigs'],
 function (require) {
 	'use strict';
 
-	var Marionette = require('backbone.marionette');
+	var Marionette = require('marionette');
 	var Gigs = require('collections/Gigs');
 
-	return Marionette.Controller.extend({
+	var GigStore = Marionette.Controller.extend({
 		
 		getRecentUpcomingGigs: function() {
 			var gigs = new Gigs();
@@ -15,8 +15,8 @@ function (require) {
 			gigs.setLimit(5);
 
 			gigs.fetch({
-				success: function(data) {
-					deferred.resolve(data);
+				success: function(collection, response, options) {
+					deferred.resolve(collection);
 				},
 			});
 
@@ -24,4 +24,5 @@ function (require) {
 		},
 	});
 
+	return new GigStore();
 });
--- web-dev/core/app/views/home/HomeLayout.js
+++ web-dev/core/app/views/home/HomeLayout.js
@@ -1,8 +1,18 @@
-define( [ 'App', 'marionette', 'handlebars', 'views/calendar/RecentUpcomingGigsView', 
+define( [ 'require', 'App', 'marionette', 'handlebars', 'views/calendar/RecentUpcomingGigsView',
     'views/show/ShowsListView', "views/guestbook/PicksView", 'text!./templates/home.html',
-    "handlebars-helpers-my"],
-    function( App, Marionette, Handlebars, HomeCalendarView,
-        ShowsListView, GuestbookPicksView, template) {
+    "handlebars-helpers-my", 'controllers/GigsStore', 'Constants'],
+    function(require) {
+
+        var Marionette = require('marionette'),
+            App = require('App'),
+            Handlebars = require('handlebars'),
+            HomeCalendarView = require('views/calendar/RecentUpcomingGigsView'),
+            ShowsListView = require('views/show/ShowsListView'),
+            GuestbookPicksView = require("views/guestbook/PicksView"),
+            GigsStore = require('controllers/GigsStore'),
+            Constants = require('Constants'),
+            template = require('text!./templates/home.html');
+
         return Marionette.LayoutView.extend( {
             template: Handlebars.compile(template),
 
@@ -12,20 +22,24 @@ define( [ 'App', 'marionette', 'handlebars', 'views/calendar/RecentUpcomingGigsV
                 guestbook: '.guestbook-picks'
             },
 
-            events: {
-            },
-
-            initialize: function(options) {
-            },
-
-            onRender: function(e) {
+            onShow: function(e) {
                 this.renderCalendar();
                 this.renderShows();
                 this.renderGuestbookPicks();
             },
             
             renderCalendar: function() {
-                this.calendar.show(new HomeCalendarView());
+                var spinner = new Spinner(Constants.SPINNER_TINY).spin();
+                $(this.calendar.$el.selector).append(spinner.el);
+
+                var fetchingGigs = GigsStore.getRecentUpcomingGigs();
+                var that = this;
+
+                $.when(fetchingGigs).done(function(fetchedGigs) {
+                    that.calendar.show(new HomeCalendarView({
+                        collection: fetchedGigs
+                    }));
+                });
             },
 
             renderShows: function() {
