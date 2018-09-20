b69f3e075ffb8636af6f601141195fcbea9275b6
Luca Marchesini <lmarchesini@kaliop.com>
2018-09-20 21:03:00 +0200

    Better gig sorting

--- data/gigs.api.js
+++ data/gigs.api.js
@@ -1,25 +1,21 @@
 import gigsJson from "./gigs.json";
 
-const dateFormat = "YYYY-MM-DD";
+const dateFormat = "YYYY-MM-DD HH:mm:ss";
 const gigs = Array.from(gigsJson).sort((a, b) => {
-  const dateA = moment(a.date, dateFormat);
-  const dateB = moment(b.date, dateFormat);
+  const dateA = moment(`${a.date} ${a.time}`, dateFormat);
+  const dateB = moment(`${b.date} ${b.time}`, dateFormat);
 
   if (dateA.unix() === dateB.unix()) {
     return 0;
   }
-  if (dateA.unix() < dateB.unix()) {
+  if (dateA.unix() > dateB.unix()) {
     return -1;
   }
-  if (dateA.unix() > dateB.unix()) {
+  if (dateA.unix() < dateB.unix()) {
     return 1;
   }
 });
 
-export const latest = limit => {
-  gigs;
-};
-
-const getNextUpcomingIdx = gigs => {
-  const now = moment.unix();
+export const some = limit => {
+  return gigs.slice(0, limit);
 };
