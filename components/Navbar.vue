e3f6c39b67f30e6c883ea42021ca313c142ea080
Luca Marchesini <lmarchesini@kaliop.com>
2018-09-21 22:36:07 +0200

    Guestbook OK

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -9,14 +9,9 @@
     <b-collapse is-nav id="nav_collapse">
       <b-navbar-nav>
         <b-nav-item href="/agenda">Agenda</b-nav-item>
-        <b-nav-item-dropdown text="Spectacles" right>
-          <b-dropdown-item href="#">Luca Vide son Sac</b-dropdown-item>
-          <b-dropdown-item href="#">La Danse des Sorcières</b-dropdown-item>
-          <b-dropdown-item href="#">Le Voyage en Europe</b-dropdown-item>
-          <b-dropdown-item href="#">Barbenuit</b-dropdown-item>
-        </b-nav-item-dropdown>
-        <b-nav-item href="#">Livre d'Or</b-nav-item>
-        <b-nav-item href="#">Contact</b-nav-item>
+        <b-nav-item href="/shows">Spectacles</b-nav-item>
+        <b-nav-item href="/guestbook">Livre d'Or</b-nav-item>
+        <b-nav-item href="/contact">Contact</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
---
+++ pages/guestbook.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="Guestbook">
+    <header>
+      <b-img src="/headers/header-guestbook.jpg" fluid></b-img>
+    </header>
+    <div class="container">
+      <section class="claims">
+          <div class="claim">
+            <h1 class="claim-txt">
+              <span class="icon-quote-left pull-left"></span>
+              <em>Nous sommes vraiment très très contents de l'intervention de Luca! Et nous le remercions une fois de plus pour sa bonne humeur et son professionnalisme.</em>
+            </h1>
+            <h3 class="claim-author">Nathalie Destre</h3>
+            <p class="claim-organization">Association ¡Vamos! - Marvejols</p>
+            <hr>
+          </div>
+          <div class="claim">
+            <h1 class="claim-txt">
+              <span class="icon-quote-left pull-left"></span><em>La tournée a été très appréciée et je suis sûre qu'elle aura des retombées positives pour vous.</em>
+            </h1>
+            <h3 class="claim-author">Hélène Larose</h3>
+            <p class="claim-organization">Chef de Service - Médiathèque départementale - Hérault</p>
+            <hr>
+          </div>
+          <div class="claim">
+            <h1 class="claim-txt">
+              <span class="icon-quote-left pull-left"></span><em>Luca nous a transportés dans son voyage. Il y a eu beaucoup de rires et aussi beaucoup d’interactions! Il a su s’adapter au public, à savoir des adultes handicapés mentaux. Il a été très disponible afin de bien préparer son intervention et de l’inclure au mieux dans notre soirée.</em>
+            </h1>
+            <h3 class="claim-author">Julie Rodriguez</h3>
+            <p class="claim-organization">Educatrice - Montpellier</p>
+            <hr>
+          </div>
+          <div class="claim">
+            <h1 class="claim-txt">
+              <span class="icon-quote-left pull-left"></span>
+              <em>Sous une grande église, sur un petit banc, il conte.
+              Il conte les sorcières, les humains, les folies, les rêves, les lutins, les peurs et les envies.
+              Il conte et nous rions, nous écoutons attentivement, avec joie. Et je les vois les sourires des gens, je les entends leurs rires, et je sens l'émotion qui est parmi nous, grâce à ce moment de partage si beau que nous vivons.
+              Merci Luca pour ta générosité et ce beau voyage!
+              Sous une grande église, sur un petit banc, un formidable conteur conte.</em>
+            </h1>
+            <h3 class="claim-author">Lou Barriol</h3>
+            <p class="claim-organization">Chargée de Programmation Artistique - Festival des 4 Saisons - Saint-Jean-de-la-Blaquière (34)</p>
+            <hr>
+          </div>
+          <div class="claim">
+            <h1 class="claim-txt">
+              <span class="icon-quote-left pull-left"></span>
+              <em>Très jolie balade de contes de sorcières et d'ogres ponctuées d'intonations, bruitages ainsi que d'expressions en langue italienne.
+              Prestation interactive vivante, public enchanté sous le charme de l'intervenant. Grand Bravo!</em>
+            </h1>
+            <h3 class="claim-author">Latifa Akam</h3>
+            <p class="claim-organization">Bibliothécaire à la Médiathèque d'Agglomération Aimé Césaire - Castelnau-le-lez</p>
+            <hr>
+          </div>
+        </section>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+header {
+  width: 100%;
+  position: relative;
+  max-width: 1366px;
+  margin: auto;
+  box-shadow: 0 0 20px 0 #656565;
+  margin-bottom: 40px;
+}
+.claims {
+  font-family: Georgia, serif;
+  width: 70%;
+  margin: auto;
+}
+.claim {
+  margin-top: 30px;
+
+  .claim-txt {
+    font-family: Georgia, serif;
+    text-transform: none;
+    color: #444;
+  }
+
+  .claim-author,
+  .claim-organization {
+    text-align: right;
+    color: $color_darkgrey;
+  }
+}
+</style>
