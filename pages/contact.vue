c12e3b4c2676e0d7815c76f41a94848c18f96b8f
Luca Marchesini <lmarchesini@kaliop.com>
2018-09-28 11:34:06 +0200

    Mailing List contact form DONE

--- pages/contact.vue
+++ pages/contact.vue
@@ -5,13 +5,15 @@
     </header>
     <div class="container">
       <b-row>
-        <b-col>
-          <a href="mailto:contact@luca-marchesini.com"><span class="icon-envelope"></span>&nbsp;contact@luca-marchesini.com</a>
-          <br>
-          <a href="tel:+33633596151"><span class="icon-phone"></span>&nbsp;+33 (0) 6 33 59 61 51</a>
+        <b-col md="4">
+          <div class="ContactData">
+            <a href="mailto:contact@luca-marchesini.com"><span class="icon-envelope"></span>&nbsp;contact@luca-marchesini.com</a>
+            <br>
+            <a href="tel:+33633596151"><span class="icon-phone"></span>&nbsp;+33 (0) 6 33 59 61 51</a>
+          </div>
         </b-col>
-        <b-col>
-
+        <b-col md="8">
+          <signup-form></signup-form>
         </b-col>
       </b-row>
     </div>
@@ -20,11 +22,13 @@
 
 <script>
 import Contact from "~/components/Contact.vue";
+import SignupForm from "~/components/MailingListSignupForm.vue";
 
 export default {
   name: "ContactPage",
   components: {
-    Contact
+    Contact,
+    SignupForm
   }
 };
 </script>
@@ -33,6 +37,9 @@ export default {
 header {
   box-shadow: 0 0 20px 0 #656565;
   margin-bottom: 30px;
+}
+.ContactData {
+  margin-top: 90px;
 }
 </style>
 
