5879bf8bb2414ebf71b00a5b6c3898bf5b34b4cd
Luca Marchesini <xbill82@gmail.com>
2018-09-30 13:16:21 +0200

    Update lucavidesonsac.vue

--- pages/show/lucavidesonsac.vue
+++ pages/show/lucavidesonsac.vue
@@ -7,7 +7,10 @@
         src="https://res.cloudinary.com/luca-le-conteur/image/upload/w_auto:100:1280/v1538139917/header-lucavidesonsac.jpg">
     </template>
     <template slot="intro-lead">
-      Pourquoi se priver de liberté quand on peut tout simplement raconter les histoires qui nous viennent à l’esprit? Mais c’est pas que de mon esprit que je parle, non. C’est de l’esprit (ou de l’âme) qui se dégage du moment que l’on passe ensemble, public et conteur. Parfois on dirait que les histoires étaient là avant qu’on arrive, alors on a qu'à les laisser venir. Les histoires, elles nous choisissent, c’est pour ça qu’elles nous viennent à l’esprit.
+      Pourquoi se priver de liberté quand on peut tout simplement raconter les histoires qui nous viennent à l’esprit? 
+      Mais c’est pas que de mon esprit que je parle, non. C’est de l’esprit (ou de l’âme) qui se dégage du moment que 
+      l’on passe ensemble, public et conteur. Parfois on dirait que les histoires étaient là avant qu’on arrive, alors on 
+      a qu'à les laisser venir. Les histoires, elles nous choisissent, c’est pour ça qu’elles nous viennent à l’esprit.
     </template>
     <template slot="intro-video">
       <teaser-video
@@ -15,7 +18,10 @@
         video-credits="Perrine Boyer"
       ></teaser-video>
     </template>
-    <template slot="description">Une suite d’histoires qui se dessine au moment où le spectacle commence ou bien que l’on peut piocher au préalable de mon répertoire. Si j'ai assez d’avance, je peux aussi travailler des nouvelles histoires dans le but de rapprocher la séance à un thème donné (dicté par le cadre d’un festival ou d’un événement). La durée aussi peut varier en fonction des publics, des âges ou des impératifs de l’organisation.
+    <template slot="description">Une suite d’histoires qui se dessine au moment où le spectacle commence ou bien que l’on 
+    peut piocher au préalable de mon répertoire. Si j'ai assez d’avance, je peux aussi travailler des nouvelles histoires 
+    dans le but de rapprocher la séance à un thème donné (dicté par le cadre d’un festival ou d’un événement). La durée 
+    aussi peut varier en fonction des publics, des âges ou des impératifs de l’organisation.
 Ça peut se faire en plein air, en salle ou en balade, il suffit de demander.</template>
     <template slot="info-age">public familial à partir de 4 ans.</template>
     <template slot="info-duration">approximativement 60 minutes (ça peut varier en fonction du niveau d'attention du public).</template>
