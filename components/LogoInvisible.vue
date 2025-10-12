83bd70166bde6a9ce8d3aa54d25335d2a88d644d
Luca Marchesini <xbill82@gmail.com>
2025-10-12 12:12:33 +0200

    show invisible

---
+++ components/LogoInvisible.vue
@@ -0,0 +1,117 @@
+<template>
+  <svg
+    width="103.53668mm"
+    height="26.480665mm"
+    viewBox="0 0 103.53668 26.480665"
+    version="1.1"
+    id="svg1"
+    inkscape:version="1.3.2 (091e20e, 2023-11-25)"
+    sodipodi:docname="logo.svg"
+    xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+    xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:svg="http://www.w3.org/2000/svg"
+  >
+    <sodipodi:namedview
+      id="namedview1"
+      pagecolor="#ffffff"
+      bordercolor="#000000"
+      borderopacity="0.25"
+      inkscape:showpageshadow="2"
+      inkscape:pageopacity="0.0"
+      inkscape:pagecheckerboard="0"
+      inkscape:deskcolor="#d1d1d1"
+      inkscape:document-units="mm"
+      inkscape:zoom="1.6792274"
+      inkscape:cx="191.15934"
+      inkscape:cy="47.938713"
+      inkscape:window-width="1472"
+      inkscape:window-height="906"
+      inkscape:window-x="0"
+      inkscape:window-y="34"
+      inkscape:window-maximized="0"
+      inkscape:current-layer="layer1"
+    />
+    <defs id="defs1" />
+    <g
+      inkscape:label="Calque 1"
+      inkscape:groupmode="layer"
+      id="layer1"
+      transform="translate(-50.396534,-122.81047)"
+    >
+      <text
+        xml:space="preserve"
+        style="
+          font-size: 25.4px;
+          line-height: 0.9;
+          font-family: 'Belta Light';
+          -inkscape-font-specification: 'Belta Light, ';
+          text-align: end;
+          text-anchor: end;
+          fill: #f4eed7;
+          stroke-width: 0.365001;
+          stroke-linecap: round;
+          stroke-miterlimit: 1.5;
+        "
+        x="155.06987"
+        y="140.59047"
+        id="text1"
+      >
+        <tspan
+          sodipodi:role="line"
+          id="tspan1"
+          style="
+            font-style: normal;
+            font-variant: normal;
+            font-weight: normal;
+            font-stretch: normal;
+            font-family: Linea;
+            -inkscape-font-specification: Linea;
+            fill: #000000;
+            stroke-width: 0.365;
+          "
+          x="155.06987"
+          y="140.59047"
+        >
+          l'invisible
+        </tspan>
+      </text>
+      <text
+        xml:space="preserve"
+        style="
+          font-size: 6.37024px;
+          line-height: 0.9;
+          font-family: Linea;
+          -inkscape-font-specification: Linea;
+          text-align: end;
+          text-anchor: end;
+          fill: #000000;
+          stroke-width: 0.0915412;
+          stroke-linecap: round;
+          stroke-miterlimit: 1.5;
+        "
+        x="153.75214"
+        y="148.2719"
+        id="text2"
+      >
+        <tspan
+          sodipodi:role="line"
+          id="tspan2"
+          style="
+            font-style: italic;
+            font-variant: normal;
+            font-weight: normal;
+            font-stretch: normal;
+            font-family: Compagnon;
+            -inkscape-font-specification: 'Compagnon Italic';
+            stroke-width: 0.0915412;
+          "
+          x="153.75214"
+          y="148.2719"
+        >
+          contes &amp; parfum
+        </tspan>
+      </text>
+    </g>
+  </svg>
+</template>
---
+++ pages/show/invisible.vue
@@ -0,0 +1,99 @@
+<template>
+  <show>
+    <template slot="header">
+      <div class="LogoWrapper">
+        <LogoInvisible class="LogoCentered" />
+      </div>
+    </template>
+    <template slot="intro-lead">
+      Les Contes et les Mythes sont un produit du rêve. Le temps de la veillée
+      contée, comme le temps des rêves, est un moment privilégié dans lequel
+      nous sommes en contact avec tout ce qui vit à l’intérieur de nous-mêmes,
+      ce bouillonnement mystérieux au dessus de notre sphère consciente. Mais
+      nos sens aussi peuvent nous projeter dans cet espace et, parmi eux, aucun
+      n’a le pouvoir évocateur de l’olfaction. Avec L’Invisible, nous avons
+      expérimenté avec l’interaction entre l’olfaction et le conte, projetant
+      ainsi le public dans son inconscient profond.
+    </template>
+    <template slot="intro-video">
+      <teaser-video youtube-id="iIXYsUBnoSk" video-credits="Ariel F. Verba" />
+    </template>
+    <template slot="description">
+      L’Invisible est conçu comme un dialogue entre le Conte et le Parfum, qui
+      peut se déployer dans une salle de spectacle ou en une itinérance à
+      travers un lieu articulé sur plusieurs espaces. Luca interprète des récits
+      issus de traditions orales variées, mais aussi des nouvelles
+      contemporaines. Pendant les histoires, des diﬀuseurs émanent subtilement
+      les parfums créés spécialement pour chaque conte, rencontrant le public.
+    </template>
+    <template slot="info-age">Tout public à partir de 11 ans</template>
+    <template slot="info-duration">Approximativement 1h</template>
+    <template slot="info-more">
+      <h1>Crédits</h1>
+      <dl>
+        <dt>Ecrit et conté par</dt>
+        <dd>Luca Marchesini (Le Canapé dans l'Arbre)</dd>
+        <dt>Parfums créés par</dt>
+        <dd>Gaël Montero (Givaudan)</dd>
+        <dt>Soutien académique</dt>
+        <dd>Patricio Nusshold (UPVM Montpellier 3)</dd>
+        <dt>Coaching teatral</dt>
+        <dd>Adélaïde Héliot</dd>
+        <dt>Charte Graphique</dt>
+        <dd>Nicolas Froment (studio cartblanch, Toulouse)</dd>
+      </dl>
+    </template>
+    <template slot="btn-dl-file">
+      <b-button
+        class="mb-3"
+        to="/docs/presenatation-invisible.pdf"
+        variant="primary"
+        target="_blank"
+        ><span class="glyphicon glyphicon-file"></span> Télécharger la
+        présenation</b-button
+      >
+      <br />
+      <b-button
+        to="/docs/dossier-invisible.pdf"
+        variant="primary"
+        target="_blank"
+        ><span class="glyphicon glyphicon-file"></span> Télécharger le
+        dossier</b-button
+      >
+    </template>
+  </show>
+</template>
+
+<script>
+import LogoInvisible from "~/components/LogoInvisible.vue";
+import Show from "~/components/Show.vue";
+import TeaserVideo from "~/components/TeaserVideo.vue";
+
+export default {
+  name: "InvisiblePage",
+  components: {
+    LogoInvisible,
+    Show,
+    TeaserVideo,
+  },
+};
+</script>
+
+<style scoped>
+.LogoWrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  min-height: 200px;
+}
+
+.LogoCentered {
+  width: auto;
+  height: auto;
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+}
+</style>
